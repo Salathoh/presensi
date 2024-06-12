46930f4446f4406b40fc34103e6946fd62a740dd
Salathoh <muhammadzuhrifebriansyah@gmail.com>
2024-06-12 17:12:34 +0700

    bismillah

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:bismillah/screen/Dashboard.dart';
+import 'package:bismillah/screen/Login.dart';
+import 'package:bismillah/screen/SplashScreen.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Aplikasi Polbeng',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    );
+  }
+}
